af991c8ca6dc39093ece601d2ebd665c3e81e309
dobr1kq <08022005dma@gmail.com>
2024-12-10 19:28:24 +0200

    database

---
+++ BookLibrary/Infrastructure/Persistence/Migrations/20241210172047_Initial.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "authors",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    last_name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    country = table.Column<string>(type: "varchar(100)", nullable: false),
+                    date_of_birth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_authors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_books", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "genres",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    genre_name = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_genres", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "librarians",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    last_name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    telephone_number = table.Column<string>(type: "varchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_librarians", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "loans",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    reader_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    book_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    librarian_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    loan_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    return_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_loans", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "publishers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    publisher_name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    publisher_address = table.Column<string>(type: "varchar(500)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_publishers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "readers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    last_name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    telephone_number = table.Column<string>(type: "varchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_readers", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "authors");
+
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "genres");
+
+            migrationBuilder.DropTable(
+                name: "librarians");
+
+            migrationBuilder.DropTable(
+                name: "loans");
+
+            migrationBuilder.DropTable(
+                name: "publishers");
+
+            migrationBuilder.DropTable(
+                name: "readers");
+        }
+    }
+}
